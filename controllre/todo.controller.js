4b1419050c65a7b1384b55e59326a98fb3b2abea
Your Name <you@example.com>
2024-06-08 12:26:36 +0530

    initial

---
+++ controllre/todo.controller.js
@@ -0,0 +1,20 @@
+const Todo = require("../modles/Todo")
+const asyncHandler = require("express-async-handler")
+
+
+exports.getAllTodos = asyncHandler(async (req, res) => {
+    const result = Todo.find()
+    res.json({ message: "Todo Fetch Success", result })
+})
+exports.addTodo = asyncHandler(async (req, res) => {
+    await Todo.create(req.body)
+    res.json({ message: "Todo add Success" })
+})
+exports.updateTodo = asyncHandler(async (req, res) => {
+    await Todo.findByIdAndUpdate(req.params.id, req.body)
+    res.json({ message: "Todo update Success" })
+})
+exports.deleteTodo = asyncHandler(async (req, res) => {
+    await Todo.findByIdAndDelete(req.params.id)
+    res.json({ message: "Todo delete Success" })
+})
